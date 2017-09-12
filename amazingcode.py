3464404cf720a2c70fc1e7b2afc327d298d2a786
cshackerpro <brianna.loomis@duke.edu>
2017-09-12 15:08:37 -0400

    Amazing code that works. Fixes #1

--- amazingcode.py
+++ amazingcode.py
@@ -5,8 +5,8 @@ def amazingcode(num1, num2):
         raise ValueError('Type Error: first arg is not an int')
     if (type(num2) != int):
         raise ValueError('Type Error: second arg is not an int')
-    summation = num1 + num2
-    if (summation < 0):
+    sum_num = num1 + num2
+    if (sum_num < 0):
         return 0
     else:
-        return summation
+        return sum_num
